7f72eeb470d524c47a2822e5ab647b274372fd29
Aman <132681496+tutankhAman@users.noreply.github.com>
2026-02-04 16:59:49 +0530

    feat: refactor App component and add marquee animation styles

--- src/app.tsx
+++ src/app.tsx
@@ -1,46 +1,12 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { HeroSection } from "./components/homepage/hero.section";
+import { RetroNavbar } from "./components/ui/retro-navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" rel="noopener" target="_blank">
-          <img
-            alt="Vite logo"
-            className="logo"
-            height="32"
-            src={viteLogo}
-            width="32"
-          />
-        </a>
-        <a href="https://react.dev" rel="noopener" target="_blank">
-          <img
-            alt="React logo"
-            className="logo react"
-            height="32"
-            src={reactLogo}
-            width="36"
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)} type="button">
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <RetroNavbar />
+      <HeroSection />
+    </main>
   );
 }
 
